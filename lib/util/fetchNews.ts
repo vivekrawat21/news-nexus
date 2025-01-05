37cadb51e456b1ab096d50af516255b36b5a3d81
vivekrawat21 <vivekrwt2111@gmail.com>
2025-01-05 19:18:23 +0530

    refactor: replace axios with fetch for news API calls and improve error handling

--- lib/util/fetchNews.ts
+++ lib/util/fetchNews.ts
@@ -1,13 +1,12 @@
-import axios from 'axios';
-
-
 export const fetchNews = async () => {
 
   try {
-    const response = await axios.get(
+    const response = await fetch(
       `https:newsapi.org/v2/everything?q=tesla&from=2024-12-05&sortBy=publishedAt&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
     );
-    return response.data.articles;
+    const data = await response.json();
+    console.log(data)
+    return data.articles;
   } catch (error) {
     console.error("Error fetching news:", error);
     return [];
